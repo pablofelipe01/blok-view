a94a1fc2a5d9c503db07fcda1233a94b980d92ec
Pablo Acebedo <pablofelipe@me.com>
2022-04-18 15:06:00 -0500

    first function smart contract

--- contract/assembly/index.ts
+++ contract/assembly/index.ts
@@ -1,5 +1,34 @@
 
 
-import { Context, logging, storage } from 'near-sdk-as'
+import { Context, logging, storage, PersistentMap } from 'near-sdk-as';
+
+const recipientList = new PersistentMap<string,string[]>('RL');
+const totalSent = new PersistentMap<string,i32[]>('TS');
+
+export function addFunds (recipient: string, amount: i32): void{
+  if (recipientList.contains(Context.sender)) {
+
+    let getList=recipientList.getSome(Context.sender)
+    let getTotals=totalSent.getSome(Context.sender)
+
+    if(getList.includes(recipient)){
+      let getIndex = getList.indexOf(recipient)
+      let oldTotal = getTotals[getIndex]
+      let newTotal = oldTotal + amount
+      getTotals[getIndex] = newTotal
+      totalSent.set(Context.sender, getTotals)
+    } else {
+      getList.push(recipient)
+      recipientList.set(Context.sender, getList)
+      getTotals.push(amount)
+      totalSent.set(Context.sender, getTotals)
+    }
+  }else{
+    recipientList.set(Context.sender,[recipient])
+    totalSent.set(Context.sender, [amount])
+  }
+}
+
+
 
 
---
+++ src/Components/SendTokens.js
@@ -0,0 +1,21 @@
+import React, { useState, useEffect } from 'react';
+import PropTypes from 'prop-types';
+
+const SendTokens = props => {
+         
+    const [recipients, setRecipients] = useState([])
+    const [valuesSent, setValues] = useState([])
+    const [balance, changeBalance] = useState(0)
+
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+SendTokens.propTypes = {
+    
+};
+
+export default SendTokens;
